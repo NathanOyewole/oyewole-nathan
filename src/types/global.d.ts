dace5b30425cb54275a1b8807787a1f147b9bd1c
Emmanuel Oreofe Oyewole <nathanemmanueloreofe@outlook.com>
2025-10-28 18:07:08 +0100

    feat: add type declarations for CSS and SCSS file imports in Next.js

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+// Type declarations for importing CSS files as side-effects in Next.js
+// This allows `import './globals.css'` without TypeScript errors.
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
+
+export {};
